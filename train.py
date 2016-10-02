24057844d00ef1938b56c75b6d2d3b7f84ce845f
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-10-02 18:45:09 -0400

    add logging

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import argparse
 
 from data.database import Database
 from src.c3d import C3DModel
+import keras
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--db_path", dest="db_path", default="../dataset", type=str, help="dataset path")
@@ -10,12 +11,18 @@ parser.add_argument("--model", dest="model", type=str, help="model weight to be 
 parser.add_argument("--sequence_size", dest="sequence_size", default=10, type=int, help="batch size")
 batch_size = 1
 options = parser.parse_args()
-db = Database(options.db_path, options.sequence_size, batch_size=batch_size,size=321,output_size=158)
+db = Database(options.db_path, options.sequence_size, batch_size=batch_size, size=321, output_size=158)
 
-model = C3DModel(options.sequence_size,output_size=158)
+model = C3DModel(options.sequence_size, output_size=158)
 
 n_epoch = 0
 max_epoch = 10
+
+
+def logging(epoch, log):
+    with open("log.log", "w") as f:
+        f.write(epoch)
+        f.write(log)
 
 
 def get_generator():
@@ -30,6 +37,6 @@ def get_generator():
             db.next_video()
 
 
-model.get_model().fit_generator(get_generator(), db.get_total_count(), max_epoch)
+model.get_model().fit_generator(get_generator(), db.get_total_count(), max_epoch,callbacks=[keras.callbacks.ModelCheckpoint("mod.model",save_best_only=True)])
 
 model.get_model().save_weights("{}_w.h5".format(model.name))
