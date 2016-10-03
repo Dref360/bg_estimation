9f3b88a63580f712f612b3b929f8612e79cf4926
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-10-03 17:29:40 -0400

    add own CSVLogger

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,61 @@
+import keras
+from collections import Iterable, OrderedDict
+import numpy as np
+import csv
+
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+
+class CSVLogger(keras.callbacks.Callback):
+    '''Callback that streams epoch results to a csv file.
+    Supports all values that can be represented as a string,
+    including 1D iterables such as np.ndarray.
+    # Example
+        ```python
+            csv_logger = CSVLogger('training.log')
+            model.fit(X_train, Y_train, callbacks=[csv_logger])
+        ```
+    Arguments
+        filename: filename of the csv file, e.g. 'run/log.csv'.
+        separator: string used to separate elements in the csv file.
+        append: True: append if file exists (useful for continuing
+            training). False: overwrite existing file,
+    '''
+
+    def __init__(self, filename, separator=',', append=False):
+        self.sep = separator
+        self.filename = filename
+        self.append = append
+        self.writer = None
+        self.keys = None
+        super(CSVLogger, self).__init__()
+
+    def on_train_begin(self, logs={}):
+        if self.append:
+            self.csv_file = open(self.filename, 'a')
+        else:
+            self.csv_file = open(self.filename, 'w')
+
+    def on_epoch_end(self, epoch, logs={}):
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (', '.join(map(lambda x: str(x), k)))
+            else:
+                return k
+
+        if not self.writer:
+            self.keys = sorted(logs.keys())
+            self.writer = csv.DictWriter(self.csv_file, fieldnames=['epoch'] + self.keys)
+            self.writer.writeheader()
+
+        row_dict = OrderedDict({'epoch': epoch})
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+        self.writer.writerow(row_dict)
+        self.csv_file.flush()
+
+    def on_train_end(self, logs={}):
+        self.csv_file.close()
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import keras
 import numpy as np
 
 from data.database import Database
-from lib.utils import chunks
+from lib.utils import chunks, CSVLogger
 from src.c3d import C3DModel
 
 parser = argparse.ArgumentParser()
@@ -37,6 +37,6 @@ def get_generator_batched():
 
 
 model.get_model().fit_generator(get_generator_batched(), db.get_total_count(), max_epoch,
-                                callbacks=[keras.callbacks.ModelCheckpoint("mod.model", save_best_only=True)])
+                                callbacks=[keras.callbacks.ModelCheckpoint("mod.model", save_best_only=True),CSVLogger("log.csv",append=True)])
 
 model.get_model().save_weights("{}_w.h5".format(model.name))
