07df0771fe3abf3a3c42c3dee657ff74779299fa
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-10-10 21:24:55 -0400

    Better logging

--- train.py
+++ train.py
@@ -10,18 +10,36 @@ from data.database import Database
 from lib.utils import chunks, CSVLogger
 from lib.img_sim import compute_ssim
 from src.c3d import C3DModel
+from src.CRNN import CRNN
+import logging
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--db_path", dest="db_path", default="../dataset", type=str, help="dataset path")
-parser.add_argument("--model", dest="model", type=str, help="model weight to be loaded, blank if new model")
+parser.add_argument("--weight_file", dest="weight_file", type=str, help="model weight to be loaded, blank if new model")
 parser.add_argument("--sequence_size", dest="sequence_size", default=10, type=int, help="batch size")
 parser.add_argument("--batch_size", dest="batch_size", default=1, type=int, help="batch size")
 parser.add_argument("--custom_lenght", dest="custom_lenght", default=None, type=int, help="max video to look at")
 parser.add_argument("--n_epochs", dest="n_epochs", default=10, type=int, help="nb epochs")
+parser.add_argument("--method", dest="method", default="c3d", type=str, help="[c3d,crnn,vae,gan]")
 batch_size = 1
 options = parser.parse_args()
 
-model = C3DModel(options.sequence_size, batch_size=options.batch_size)
+print(vars(options))
+logging.basicConfig(filename='logging.log', level=logging.DEBUG,
+                    format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
+logging.info(vars(options))
+
+methods = ["c3d", "crnn", "vae", "gan"]
+assert options.method in methods, "Not a valid method"
+
+if options.method == "c3d":
+    model = C3DModel(options.sequence_size, batch_size=options.batch_size, weight_file=options.weight_file)
+elif options.method == "crnn":
+    model = CRNN(options.sequence_size, batch_size=options.batch_size, weight_file=options.weight_file)
+else:
+    print("{} is not available at this moment".format(options.method))
+    exit(0)
+
 db = Database(options.db_path, options.sequence_size, batch_size=options.batch_size, size=model.img_size,
               output_size=model.output_size, custom_lenght=options.custom_lenght)
 
@@ -54,7 +72,7 @@ def save_one():
     gt = 255. * np.reshape(gts, [model.output_size, model.output_size])
     cv2.imwrite("output.png", output)
     cv2.imwrite("gt.png", gt)
-    return abs(compute_ssim(output,gt,255))
+    return abs(compute_ssim(output, gt, 255))
 
 
 model.get_model().fit_generator(generator=get_generator_batched(), samples_per_epoch=db.get_total_count(),
@@ -63,9 +81,11 @@ model.get_model().fit_generator(generator=get_generator_batched(), samples_per_e
                                            CSVLogger("log.csv", append=True)])
 
 history = model.get_model().evaluate_generator(get_validation_generator_batched(), db.get_total_test_count())
+logging.info(history)
 with open("history.log", "w") as f:
     json.dump(history, f)
 
-print(save_one())
+ssms = save_one()
+logging.info("SSMS {}".format(ssms))
 
 model.get_model().save_weights("{}_w.h5".format(model.name))
