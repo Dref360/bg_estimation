3bfca059f8e6b04ebe81dfb543f1ed5242b895f3
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-10-25 10:08:12 -0400

    fix VAE, can now be saved

--- src/vae.py
+++ src/vae.py
@@ -1,6 +1,7 @@
 '''
-Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
+Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6129
 '''
+import tensorflow as tf
 from keras import backend as K
 from keras import objectives
 from keras.layers import Convolution2D, Convolution3D, MaxPooling3D, Deconvolution2D
@@ -12,7 +13,8 @@ from src.base_model import BaseModel, Relu
 
 class VAE(BaseModel):
     def __init__(self, sequence_size, img_size=321, batch_size=1, weight_file=None):
-        BaseModel.__init__(self, "CRNN", batch_size)
+        self.output_size = 118
+        BaseModel.__init__(self, "VAE", batch_size)
         self.sequence_size = sequence_size
         self.img_size = img_size
         self.build_model(loss=self.vae_loss)
@@ -20,13 +22,31 @@ class VAE(BaseModel):
         if weight_file:
             self.model.load_weights(weight_file)
 
+    def sampling(self, args):
+        latent_dim = 2
+        epsilon_std = 0.01
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(self.batch_size, latent_dim),
+                                  mean=0., std=epsilon_std)
+        return z_mean + K.exp(z_log_var) * epsilon
+
     def vae_loss(self, x, x_decoded_mean):
+        self.output_size = 118
         # NOTE: binary_crossentropy expects a batch_size by dim
         # for x and x_decoded_mean, so we MUST flatten these!
         x = K.flatten(x)
         x_decoded_mean = K.flatten(x_decoded_mean)
         xent_loss = self.output_size * self.output_size * objectives.binary_crossentropy(x, x_decoded_mean)
-        kl_loss = - 0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        z_exp = K.exp(self.z_log_var)
+        z_exp = tf.select(tf.is_nan(z_exp), -1 * tf.ones_like(z_exp), z_exp)
+        z_exp = tf.select(tf.is_inf(z_exp), tf.ones_like(z_exp), z_exp)
+        self.z_log_var = tf.select(tf.is_nan(self.z_log_var), -1 * tf.ones_like(self.z_log_var), self.z_log_var)
+        self.z_log_var = tf.select(tf.is_inf(self.z_log_var), tf.ones_like(self.z_log_var), self.z_log_var)
+
+        self.z_mean = tf.select(tf.is_nan(self.z_mean), -1 * tf.ones_like(self.z_mean), self.z_mean)
+        self.z_mean = tf.select(tf.is_inf(self.z_mean), tf.ones_like(self.z_mean), self.z_mean)
+
+        kl_loss = - 0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - z_exp, axis=-1)
         return xent_loss + kl_loss
 
     def _build_model(self):
@@ -49,24 +69,18 @@ class VAE(BaseModel):
         self.z_mean = Dense(latent_dim)(hidden)
         self.z_log_var = Dense(latent_dim)(hidden)
 
-        def sampling(args):
-            z_mean, z_log_var = args
-            epsilon = K.random_normal(shape=(self.batch_size, latent_dim),
-                                      mean=0., std=epsilon_std)
-            return z_mean + K.exp(z_log_var) * epsilon
-
         # note that "output_shape" isn't necessary with the TensorFlow backend
         # so you could write `Lambda(sampling)([z_mean, z_log_var])`
-        z = Lambda(sampling, output_shape=(latent_dim,))([self.z_mean, self.z_log_var])
+        z = Lambda(self.sampling, output_shape=(latent_dim,))([self.z_mean, self.z_log_var])
 
         # we instantiate these layers separately so as to reuse them later
         decoder_hid = Dense(intermediate_dim, activation='relu')
-        decoder_upsample = Dense(nb_filters * 14 * 14, activation='relu')
+        decoder_upsample = Dense(nb_filters * 29 * 29, activation='relu')
 
         if K.image_dim_ordering() == 'th':
-            output_shape = (self.batch_size, nb_filters, 14, 14)
+            output_shape = (self.batch_size, nb_filters, 29, 29)
         else:
-            output_shape = (self.batch_size, 14, 14, nb_filters)
+            output_shape = (self.batch_size, 29, 29, nb_filters)
 
         decoder_reshape = Reshape(output_shape[1:])
 
@@ -81,21 +95,12 @@ class VAE(BaseModel):
                                            border_mode='same',
                                            subsample=(1, 1),
                                            activation='relu')
-        if K.image_dim_ordering() == 'th':
-            output_shape = (self.batch_size, nb_filters, 29, 29)
-        else:
-            output_shape = (self.batch_size, 29, 29, nb_filters)
-        decoder_deconv_3_upsamp = Deconvolution2D(nb_filters, 3, 3,
-                                                  output_shape,
-                                                  border_mode='valid',
-                                                  subsample=(2, 2),
-                                                  activation='relu')
 
         if K.image_dim_ordering() == 'th':
             output_shape = (self.batch_size, nb_filters, 59, 59)
         else:
             output_shape = (self.batch_size, 59, 59, nb_filters)
-        decoder_deconv_4_upsamp = Deconvolution2D(nb_filters, 3, 3,
+        decoder_deconv_3_upsamp = Deconvolution2D(nb_filters, 3, 3,
                                                   output_shape,
                                                   border_mode='valid',
                                                   subsample=(2, 2),
@@ -105,7 +110,7 @@ class VAE(BaseModel):
             output_shape = (self.batch_size, nb_filters, 119, 119)
         else:
             output_shape = (self.batch_size, 119, 119, nb_filters)
-        decoder_deconv_5_upsamp = Deconvolution2D(nb_filters, 3, 3,
+        decoder_deconv_4_upsamp = Deconvolution2D(nb_filters, 3, 3,
                                                   output_shape,
                                                   border_mode='valid',
                                                   subsample=(2, 2),
@@ -121,10 +126,7 @@ class VAE(BaseModel):
         deconv_1_decoded = decoder_deconv_1(reshape_decoded)
         deconv_2_decoded = decoder_deconv_2(deconv_1_decoded)
         deconv_3_decoded = decoder_deconv_3_upsamp(deconv_2_decoded)
-        deconv_4_decoded = decoder_deconv_4_upsamp(deconv_3_decoded)
-        x_decoded_relu = decoder_deconv_5_upsamp(deconv_4_decoded)
+        x_decoded_relu = decoder_deconv_4_upsamp(deconv_3_decoded)
         x_decoded_mean_squash = decoder_mean_squash(x_decoded_relu)
-        vae = Model(inputs, x_decoded_mean_squash)
-        self.output_size = vae.get_output_shape_at(-1)[-1]
-        vae.compile(optimizer='rmsprop', loss=self.vae_loss)
+        vae = Model(input=inputs, output=x_decoded_mean_squash)
         return vae
