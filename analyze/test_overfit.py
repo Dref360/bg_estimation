e711a68f3d172bc8680cf36ff034cee2394fd2b8
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-11-23 16:26:25 -0500

    test_overfit is able to train now

--- analyze/test_overfit.py
+++ analyze/test_overfit.py
@@ -8,6 +8,7 @@ import numpy as np
 from analyze.Evaluate import Evaluate
 from data.database import Database
 from lib.decorator import GeneratorLoop
+from lib.utils import chunks
 from src.CRNN import CRNN
 from src.c3d import C3DModel
 from src.unet import UNETModel
@@ -15,7 +16,7 @@ from src.vae import VAE
 from src.vgg3d import VGG3DModel
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("weight_file", type=str, help="model weight to be loaded, blank if new model")
+parser.add_argument("--weight_file", type=str, default=None, help="model weight to be loaded, blank if new model")
 parser.add_argument("--db_path", dest="db_path", default="../../dataset", type=str, help="dataset path")
 parser.add_argument("--sequence_size", dest="sequence_size", default=10, type=int, help="batch size")
 parser.add_argument("--batch_size", dest="batch_size", default=1, type=int, help="batch size")
@@ -50,19 +51,40 @@ output_file = "output/out{}_{}.png"
 
 db = Database(options.db_path, options.sequence_size, batch_size=options.batch_size, size=model.img_size,
               output_size=model.output_size)
+max_epoch = 15
+
+@GeneratorLoop
+def get_generator_batched_for_id(id, ratio):
+    for batch in chunks(db.get_datas_on_one(id)[:min(int(db.get_count_on_video(id) * ratio), options.max_length)],
+                        options.batch_size):
+        imgs, gts = zip(*batch)
+        yield model.preprocess(np.asarray([db.load_imgs(img) for img in imgs]),
+                               np.asarray([db.get_groundtruth(gt, 255.0) for gt in gts]))
+
+
+ratio = 0.7
 
 
 @GeneratorLoop
-def overfit_generator():
-    for (imgs, gt) in db.get_datas_on_one(options.videoid):
-        yield model.preprocess(np.asarray([db.load_imgs(imgs)]), db.get_groundtruth(gt, 255.0))
+def get_generator_test_batched_for_id(id, ratio):
+    max_test = db.get_count_on_video(id) - options.max_length
+    for batch in chunks(db.get_datas_on_one(id)[max(max_test, int(db.get_count_on_video(id) * ratio)):],
+                        options.batch_size):
+        imgs, gts = zip(*batch)
+        yield model.preprocess(np.asarray([db.load_imgs(img) for img in imgs]),
+                               np.asarray([db.get_groundtruth(gt, 255.0) for gt in gts]))
 
 
-outputs = model.get_model().predict_generator(overfit_generator(), db.get_count_on_video(options.videoid))
+if options.weight_file is None:
+    model.get_model().fit_generator(generator=get_generator_batched_for_id(options.videoid, ratio),
+                                    samples_per_epoch=int(db.get_count_on_video(options.videoid) * ratio),
+                                    nb_epoch=max_epoch)
+outputs = model.get_model().predict_generator(get_generator_batched_for_id(options.videoid, ratio),
+                                              int(db.get_count_on_video(options.videoid) * (1 - ratio)))
 gt = db.get_groundtruth_from_id(options.videoid)
 acc = []
 for i, output in enumerate(outputs):
-    cv2.imwrite(output_file.format(options.method, i), output.reshape([model.output_size, model.output_size]))
+    cv2.imwrite(output_file.format(options.method, i), output.reshape([model.output_size, model.output_size]) * 255.)
     acc += Evaluate(gt, output)
 
 print(acc)
